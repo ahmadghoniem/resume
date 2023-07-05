ef3adf4238150fbbaf57cb1460977c16ba68c8c4
kontb2a22 <ahmedxd.2009@gmail.com>
2023-07-05 13:35:01 +0300

    updated control center
    updated control center

--- src/Components/ControlCenter.jsx
+++ src/Components/ControlCenter.jsx
@@ -2,7 +2,10 @@ import DownloadIcon from "./DownloadIcon";
 const ControlCenter = () => {
   return (
     <div className="fixed inset-0 bottom-4 mx-auto mt-auto flex h-fit w-3/5 flex-row justify-around rounded-lg border  bg-white py-2 opacity-60 shadow-md transition-opacity duration-150 hover:opacity-100 print:hidden">
-      <span>WIP🛠️</span>
+      <div className="flex flex-col">
+        <span className="text-sm">WIP🛠️</span>
+        <p className="text-xs">Built with ReactJS and TailwindCSS</p>
+      </div>
       <button
         className="rounded-md bg-[#D9E3EB] px-4 py-2 text-white"
         onClick={() => window.print()}
